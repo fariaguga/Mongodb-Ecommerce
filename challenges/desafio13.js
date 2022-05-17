33924a8dc1ed522179bbbfa8957eb1d9f1717be5
Faria <fariaguga@hotmail.com>
2022-05-17 11:57:39 -0300

    feat: add Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+    { criadoPor: null },
+    { $set: { criadoPor: "Ronald Mcdonald" } }
+  );
+  
+db.produtos.find({}, {
+     nome: 1,
+     criadoPor: 1,
+     _id: 0,
+});
