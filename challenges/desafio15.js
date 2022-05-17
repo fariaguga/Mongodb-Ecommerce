a6b9d79b1ccf48dadf711c148944723141feefb4
Faria <fariaguga@hotmail.com>
2022-05-17 11:57:52 -0300

    feat: add Desafio 15

---
+++ challenges/desafio15.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+    { avaliacao: null },
+    { $set: { avaliacao: 0 } }
+  );
+
+db.produtos.updateMany(
+    { tags: { $eq: "bovino"} },
+    { $inc: { avaliacao: 5 } }
+  );
+
+db.produtos.updateMany(
+    { tags: { $eq: "ave"} },
+    { $inc: { avaliacao: 3 } }
+  );
+
+db.produtos.find({}, {
+     nome: 1,
+     avaliacao: 1,
+     _id: 0,
+});
